bf0c02b4b6bc8f495d3ee6dbaeb9e9c4c18fb6af
Hurshal Patel <hurshal@memsql.com>
2015-08-09 14:13:57 -0700

    clean up imports

--- src/main/scala/com/memsql/spark/examples/MemSQLETLApp.scala
+++ src/main/scala/com/memsql/spark/examples/MemSQLETLApp.scala
@@ -3,16 +3,12 @@ package com.memsql.spark.examples
 import java.text.SimpleDateFormat
 import java.util.Calendar
 
-import com.memsql.spark.connector._
-import com.memsql.spark.etl.api.ETLPipeline
-import com.memsql.spark.etl.api.MemSQLLoader
 import com.memsql.spark.etl.api.Extractor
 import com.memsql.spark.etl.api.Transformer
-import com.memsql.spark.etl.api.Loader
-import com.memsql.spark.etl.api.configs.{UserTransformConfig, UserExtractConfig, PhaseConfig}
+import com.memsql.spark.etl.api.configs.PhaseConfig
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
-import org.apache.spark.streaming.dstream.{DStream, InputDStream}
+import org.apache.spark.streaming.dstream.InputDStream
 import org.apache.spark.streaming.{StreamingContext, Time}
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.SQLContext
@@ -26,7 +22,6 @@ class MemSQLExtractor extends Extractor[Long] {
       override def start(): Unit = {}
 
       override def compute(validTime: Time): Option[RDD[Long]] = {
-        Console.println(s"EXTRACT: ${config.asInstanceOf[UserExtractConfig]}")
         Some(ssc.sparkContext.parallelize(Seq.fill(5)(Calendar.getInstance.getTimeInMillis)))
       }
     }
@@ -35,7 +30,6 @@ class MemSQLExtractor extends Extractor[Long] {
 
 class MemSQLTransformer extends Transformer[Long] {
   override def transform(sqlContext: SQLContext, from: RDD[Long], config: PhaseConfig): DataFrame = {
-    Console.println(s"TRANSFORM: ${config.asInstanceOf[UserTransformConfig]}")
     val dateFormat = new SimpleDateFormat()
     val transformed = from.map { x =>
       Row(dateFormat.format(x))
