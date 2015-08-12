4dfd840360a2351db9d2722de8fe1937a75b6d7e
JoYo <jvictor@memsql.com>
2015-08-12 18:08:12 +0000

    TransformerConfiguredByMap
    Summary: Just making a common pattern easier for gboppers.
    
    Test Plan: litterbug
    
    Reviewers: wsong, carl, hurshal
    
    Subscribers: engineering-list
    
    Differential Revision: http://grizzly.memsql.com/D11793

---
+++ etlLib/src/main/scala/com/memsql/spark/etl/api/TransformerConfiguredByMap.scala
@@ -0,0 +1,27 @@
+package com.memsql.spark.etl.api
+
+import com.memsql.spark.etl.api.configs.PhaseConfig
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.`type`.TypeReference;
+
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.rdd.RDD
+import com.memsql.spark.etl.api.configs.PhaseConfig
+import com.memsql.spark.etl.api.configs.UserTransformConfig
+
+
+trait TransformerConfiguredBJSON[S] extends Transformer[S] {
+  def transform(rdd: RDD[S], sqlContext: SQLContext, config: PhaseConfig): DataFrame = {
+    val mapper = new ObjectMapper()
+    mapper.registerModule(DefaultScalaModule)
+    val configMap: Map[String,String] = mapper.readValue(config.asInstanceOf[UserTransformConfig].value, classOf[Map[String,String]])
+    transform(rdd, sqlContext, configMap)
+  }
+  
+  def transform(rdd: RDD[S], sqlContext: SQLContext, config: Map[String,String]): DataFrame
+}
