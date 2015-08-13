cf5f88cfbfd3653ba21cc7b92015035372389771
JoYo <jvictor@memsql.com>
2015-08-13 17:21:30 +0000

    DataFrameBuilder
    Summary:
    In an effort to make user code beautiful/config-issue-proof while superapp-ing the litterbug, I invented a little utility.
    I want to avoid
     1) the user worrying about lining up columns and getting the order right.
     2) the user needing to modify two places when the schema changes.
    
    I'll include example code from litterbug
    
    Test Plan: litterbug
    
    Reviewers: hurshal, carl, wsong
    
    Subscribers: engineering-list
    
    Differential Revision: http://grizzly.memsql.com/D11814

--- connectorLib/src/main/scala/com/memsql/spark/context/MemSQLSQLContext.scala
+++ connectorLib/src/main/scala/com/memsql/spark/context/MemSQLSQLContext.scala
@@ -5,6 +5,7 @@ import com.memsql.spark.connector.dataframe.MemSQLDataFrame
 
 import org.apache.spark._
 import org.apache.spark.sql._
+import org.apache.spark.sql.types._
 
 import scala.util.Random
 
@@ -12,23 +13,22 @@ import scala.util.Random
  * A convenience class extending Spark's SQLContext.
  * Makes it easy to create dataframes from MemSQL.
  */
-class MemSQLSQLContext(sparkContext: MemSQLSparkContext) extends SQLContext(sparkContext)
-{
-    def CreateDataFrameFromMemSQLTable(dbName: String, tableName: String) : DataFrame =
-    {
-        CreateDataFrameFromMemSQLQuery(dbName, "SELECT * FROM " + tableName)
-    }
-    def CreateDataFrameFromMemSQLQuery(dbName: String, query: String) : DataFrame =
-    {
-        val aggs = sparkContext.GetMemSQLNodesAvailableForRead
-        val agg = aggs(Random.nextInt(aggs.size))
-        MemSQLDataFrame.MakeMemSQLDF(
-          this,
-          agg._1,
-          agg._2,
-          sparkContext.GetUserName,
-          sparkContext.GetPassword,
-          dbName,
-          query)
-    }
+class MemSQLSQLContext(sparkContext: MemSQLSparkContext) extends SQLContext(sparkContext) {
+  def createDataFrameFromMemSQLTable(dbName: String, tableName: String) : DataFrame = {
+    createDataFrameFromMemSQLQuery(dbName, "SELECT * FROM " + tableName)
+  }
+  def createDataFrameFromMemSQLQuery(dbName: String, query: String) : DataFrame = {
+    val aggs = sparkContext.GetMemSQLNodesAvailableForRead
+    val agg = aggs(Random.nextInt(aggs.size))
+    MemSQLDataFrame.MakeMemSQLDF(
+      this,
+      agg._1,
+      agg._2,
+      sparkContext.GetUserName,
+      sparkContext.GetPassword,
+      dbName,
+      query)
+  }
+  def getTableSchema(dbName: String, tableName: String) : StructType = createDataFrameFromMemSQLTable(dbName, tableName).schema
+ 
 }
---
+++ etlLib/src/main/scala/com/memsql/spark/etl/utils/DataFrameBuilder.scala
@@ -0,0 +1,47 @@
+package com.memsql.spark.etl.utils
+
+import org.apache.spark.sql.DataFrame
+import com.memsql.spark.connector._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.Row
+import org.apache.spark.SparkException
+
+/**
+ * A DataFrameBuilder is a utility for creating DataFrames in a mistake-resistant way from the executors.
+ * The schema is specified up front, and the row producing code sets values in its row.  
+ * Unset columns are left as null.
+ * Attempting to set a column not found in the schema will throw a SparkException.
+ *
+ * Example:
+ *     val dfb: = DataFrameBuilder(StructType(Array(StructField("a",StringType,true),StructField("b",IntegerType,true))))
+ *     val rowRDD = rdd.map { r =>
+ *       val rb = dfb.startRow
+ *       ...
+ *       rb.set("a",...)
+ *         .set("b",...)
+ *         .finishRow
+ *     }
+ *     dfb.finishDataFrame(sqlContext, rowRDD)
+ *
+ * see also: `MemSQLSQLContext::getTableSchema`
+ */
+case class DataFrameBuilder(schema: StructType) extends Serializable {
+  val colNames: Array[String] = schema.map((col: StructField) => col.name).toArray
+  def startRow: RowBuilder = RowBuilder(colNames)
+  def finishDataFrame(sqlContext: SQLContext, rdd: RDD[Row]): DataFrame = sqlContext.createDataFrame(rdd, schema)
+}
+
+case class RowBuilder(colNames: Array[String]) {
+  private val row: Array[Any] = colNames.map((x:String) => null)
+  def set(name: String, value: Any): RowBuilder = {
+    val ix = colNames.indexOf(name)
+    if (ix == -1) {
+      throw new SparkException("Tried to set row " + name + ", which does not exist")
+    }
+    row(ix) = value
+    this
+  }
+  def finishRow: Row = Row.fromSeq(row)
+}
