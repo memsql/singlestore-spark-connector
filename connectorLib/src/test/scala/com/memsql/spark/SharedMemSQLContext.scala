72cda330f2cd2be339c785b3b06b944595dc643a
John Bowler <john@memsql.com>
2015-12-07 13:40:25 -0800

    Added ScalaTest to connectorLib
    Summary:
    * Added SharedMemSQLContext, a before-and-after mixin for ScalaTest unit testing
    * Added MemSQLPushdownStrategySpec, a unit test for MemSQLPushdownStrategy with
      a single example assertion
    
    Test Plan: Local
    
    Reviewers: hurshal, ankur, wsong, carl
    
    Reviewed By: carl
    
    Subscribers: engineering-list
    
    Differential Revision: http://grizzly.memsql.com/D13399

---
+++ connectorLib/src/test/scala/com/memsql/spark/SharedMemSQLContext.scala
@@ -0,0 +1,60 @@
+package com.memsql.spark
+
+import java.net.InetAddress
+import java.security.MessageDigest
+
+import com.memsql.spark.connector.MemSQLConnectionPool
+import com.memsql.spark.connector.util.JDBCImplicits._
+import com.memsql.spark.connector.util.MemSQLConnectionInfo
+import org.apache.spark.sql.memsql.MemSQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+import org.scalatest.{BeforeAndAfterAll, Suite}
+
+///** Shares a local `MemSQLContext` between all tests in a suite and closes it at the end */
+trait SharedMemSQLContext extends BeforeAndAfterAll {self: Suite =>
+
+  val dbName: String = {
+    // Generate a unique database name based on this machine
+    // This is so that multiple people can run tests against
+    // the same MemSQL cluster.
+    val hostMD5 = MessageDigest.getInstance("md5").digest(
+      InetAddress.getLocalHost.getAddress)
+    "connector_tests_" + hostMD5.slice(0, 2).map("%02x".format(_)).mkString
+  }
+
+  val masterConnectionInfo: MemSQLConnectionInfo =
+    MemSQLConnectionInfo("127.0.0.1", 3306, "root", "", dbName) // scalastyle:ignore
+
+  val conf = new SparkConf()
+    .setAppName("MemSQL Connector Test")
+    .setMaster("local")
+    .set("memsql.host", masterConnectionInfo.dbHost)
+    .set("memsql.port", masterConnectionInfo.dbPort.toString)
+    .set("memsql.user", masterConnectionInfo.user)
+    .set("memsql.password", masterConnectionInfo.password)
+    .set("memsql.defaultDatabase", masterConnectionInfo.dbName)
+
+  val sc: SparkContext = new SparkContext(conf)
+  val msc: MemSQLContext = new MemSQLContext(sc)
+
+  override def beforeAll() {
+    recreateDatabase
+    super.beforeAll()
+  }
+
+  override def afterAll() {
+    sc.stop()
+    // To avoid Akka rebinding to the same port, since it doesn't unbind immediately on shutdown
+    System.clearProperty("spark.driver.port")
+    super.afterAll()
+  }
+
+  def recreateDatabase: Unit = {
+    MemSQLConnectionPool.withConnection(masterConnectionInfo.copy(dbName=""))(conn => {
+      conn.withStatement(stmt => {
+        stmt.execute("DROP DATABASE IF EXISTS " + dbName)
+        stmt.execute("CREATE DATABASE IF NOT EXISTS " + dbName)
+      })
+    })
+  }
+}
---
+++ connectorLib/src/test/scala/org/apache/spark/sql/memsql/MemSQLPushdownStrategySpec.scala
@@ -0,0 +1,27 @@
+package org.apache.spark.sql.memsql
+
+import com.memsql.spark.SharedMemSQLContext
+import com.memsql.spark.connector.sql.TableIdentifier
+import com.memsql.spark.pushdown.MemSQLPushdownStrategy
+import org.apache.spark.sql.execution.SparkPlan
+import org.apache.spark.sql.execution.datasources.LogicalRelation
+import org.scalatest.FlatSpec
+
+class MemSQLPushdownStrategySpec extends FlatSpec with SharedMemSQLContext {
+
+  "MemSQLPushdownStrategy" should "output a plan" in {
+    val strategy = new MemSQLPushdownStrategy(sc)
+
+    val cluster = msc.getMemSQLCluster
+    val tableIdent = TableIdentifier(msc.getDatabase, "foo")
+
+    cluster.createTable(tableIdent, Nil)
+    val memSQLRelation = MemSQLRelation(cluster, tableIdent, msc)
+
+    val logicalPlan = LogicalRelation(memSQLRelation)
+    val physicalPlan: Seq[SparkPlan] = strategy(logicalPlan)
+
+    assert(!physicalPlan.isEmpty)
+  }
+
+}
