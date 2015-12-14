60f254770b1cd089734291a51b2e6224987194a3
Carl Sverre <accounts@carlsverre.com>
2015-12-14 12:58:28 -0800

    updating tests to actually refer to real tests

--- dockertest/spark_submit_test.py
+++ dockertest/spark_submit_test.py
@@ -22,83 +22,23 @@ def do_test_spark_submit(local_context, test_name, num_aggs=0, num_leaves=1, arg
 
     assert resp.return_code == 0, "the test failed with return code %d" % resp.return_code
 
-def test_save_to_memsql_very_basic(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestSaveToMemSQLVeryBasic")
-
-def test_memsql_dataframe_very_basic(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestMemSQLDataFrameVeryBasic")
-
-def test_memsql_query_pushdown_basic(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestMemSQLQueryPushdownBasic")
-
 def test_memsql_query_expressions_binary_operators(local_context):
     do_test_spark_submit(local_context, "com.memsql.spark.TestMemSQLQueryExpressionsBinaryOperators")
 
 def test_types(local_context):
     do_test_spark_submit(local_context, "com.memsql.spark.TestTypes")
 
-def test_create_with_keys(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestCreateWithKeys")
+def test_memsql_query_pushdown_basic(local_context):
+    do_test_spark_submit(local_context, "com.memsql.spark.TestMemSQLQueryPushdown")
 
-def test_create_with_extra_cols(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestCreateWithExtraColumns")
-
-def test_save_to_memsql_empty_rows(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestSaveToMemSQLEmptyRows")
-
-def test_memsql_types(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestMemSQLTypes")
-
-def test_memsql_types_keyless(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestMemSQLTypes", args=["keyless"])
-
-def test_memsql_types_include_binary(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestMemSQLTypes", args=["includeBinary"])
-
-def test_memsql_types_keyless_include_binary(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestMemSQLTypes", args=["keyless", "includeBinary"])
-
-def test_memsql_context(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestMemSQLContextVeryBasic", num_aggs=2, num_leaves=2)
-
-def test_leaked_conns(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestLeakedConns")
+def test_relation_provider(local_context):
+    do_test_spark_submit(local_context, "com.memsql.spark.TestRelationProvider")
 
 def test_save_errors(local_context):
     do_test_spark_submit(local_context, "com.memsql.spark.TestSaveToMemSQLErrors")
 
-def test_save_no_nodes_available_error(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestSaveToMemSQLNoNodesAvailableError")
-
-def test_save_rdd_errors(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestSaveToMemSQLWithRDDErrors")
-
-def test_save_json_column(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestSaveToMemSQLJSONColumn")
-
-def test_save_binary_column(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestSaveToMemSQLBinaryColumn")
-
-def test_spark_sql_types(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestSparkSQLTypes")
-
-def test_escaped_cols(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestEscapedColumnNames")
-
-def test_dup_keys(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestSaveToMemSQLWithDupKeys")
-
-def test_dataframe_conjunctions(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestMemSQLDataFrameConjunction")
-
-def test_dist_joins(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestMemSQLRDDDistributedJoins")
-
-def test_rdd_from_prepared(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestMemSQLRDDFromSqlTemplate")
-
-def test_rdd_from_prepared_complex(local_context):
-    do_test_spark_submit(local_context, "com.memsql.spark.TestMemSQLRDDFromSqlTemplateComplex")
-
 def test_create_table_default_columns(local_context):
     do_test_spark_submit(local_context, "com.memsql.spark.TestCreateTableDefaultColumns")
+
+def test_conn_close_provider(local_context):
+    do_test_spark_submit(local_context, "com.memsql.spark.TestConnCloseProvider")
