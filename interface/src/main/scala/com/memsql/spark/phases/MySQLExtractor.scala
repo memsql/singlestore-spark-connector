9636468f3dd59f22c235cf06647aa520c5efb8e7
Allan Carroll <allan@allancarroll.com>
2016-02-10 10:27:39 -0800

    Fix config reference in MySQLExtractor
    Summary: MySQLExtractor was casting to the wrong config type.
    
    Test Plan: psytest
    
    Reviewers: john, hurshal, ankur, carl, wsong
    
    Reviewed By: carl, wsong
    
    Subscribers: engineering-list
    
    Differential Revision: https://grizzly.memsql.com/D14541

--- interface/src/main/scala/com/memsql/spark/phases/MySQLExtractor.scala
+++ interface/src/main/scala/com/memsql/spark/phases/MySQLExtractor.scala
@@ -24,18 +24,18 @@ class MySQLExtractor extends Extractor {
                     batchInterval: Long,
                     logger: PhaseLogger): Option[DataFrame] = {
     val sc = ssc.sparkContext
-    val mysqlConfig = config.asInstanceOf[UserExtractConfig]
+    val mysqlConfig = config.asInstanceOf[MySQLExtractConfig]
 
-    val host = mysqlConfig.getConfigString("host").get
-    val port = mysqlConfig.getConfigInt("port").get
-    val db_name = mysqlConfig.getConfigString("db_name").get
-    val table_name = mysqlConfig.getConfigString("table_name").get
+    val host = mysqlConfig.host
+    val port = mysqlConfig.port
+    val db_name = mysqlConfig.db_name
+    val table_name = mysqlConfig.table_name
 
     val url = s"jdbc:mysql://${host}:${port}"
     val df = sqlContext.read.format("jdbc")
       .option("url", url)
-      .option("user", mysqlConfig.getConfigString("user").get)
-      .option("password", mysqlConfig.getConfigString("password").get)
+      .option("user", mysqlConfig.user)
+      .option("password", mysqlConfig.password)
       .option("dbtable", s"${db_name}.${table_name}")
       .load()
     Some(df)
