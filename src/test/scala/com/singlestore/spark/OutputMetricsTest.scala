0bfc5c6297179c5d6e5a318bcdff68125d4fe9cc
Adalbert Makarovych <amakarovych-ua@memsql.com>
2021-02-05 11:23:40 +0200

    Forward port D45611
    Summary:
    Forward ported D45611 with a fix of OutputMetricsTest
    **Design doc/spec**:
    **Docs impact**: none
    
    Test Plan: ran locally
    
    Reviewers: carl, iblinov-ua
    
    Reviewed By: iblinov-ua
    
    Subscribers: engineering-list
    
    JIRA Issues: PLAT-4675
    
    Differential Revision: https://grizzly.internal.memcompute.com/D45613

--- src/test/scala/com/singlestore/spark/OutputMetricsTest.scala
+++ src/test/scala/com/singlestore/spark/OutputMetricsTest.scala
@@ -1,16 +1,19 @@
 package com.singlestore.spark
 
+import java.util.concurrent.CountDownLatch
 import com.github.mrpowers.spark.daria.sql.SparkSessionExt._
 import org.apache.spark.scheduler.{SparkListener, SparkListenerTaskEnd}
 import org.apache.spark.sql.types.{IntegerType, StringType}
 
 class OutputMetricsTest extends IntegrationSuiteBase {
   it("records written") {
-    var outputWritten = 0L
+    var outputWritten  = 0L
+    var countDownLatch = new CountDownLatch(1);
     spark.sparkContext.addSparkListener(new SparkListener() {
       override def onTaskEnd(taskEnd: SparkListenerTaskEnd) {
         val metrics = taskEnd.taskMetrics
         outputWritten += metrics.outputMetrics.recordsWritten
+        countDownLatch.countDown()
       }
     })
 
@@ -26,8 +29,11 @@ class OutputMetricsTest extends IntegrationSuiteBase {
       .format(DefaultSource.SINGLESTORE_SOURCE_NAME_SHORT)
       .save("metricsInts")
 
+    countDownLatch.await()
     assert(outputWritten == numRows)
+
     outputWritten = 0
+    countDownLatch = new CountDownLatch(1);
 
     val df2 = spark.createDF(
       List("st1", "", null),
@@ -38,6 +44,7 @@ class OutputMetricsTest extends IntegrationSuiteBase {
       .format(DefaultSource.SINGLESTORE_SOURCE_NAME_SHORT)
       .save("metricsStrings")
 
+    countDownLatch.await()
     assert(outputWritten == 3)
   }
 }
