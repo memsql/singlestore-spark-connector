725136295c4b2ff294df137476216255bd0c26b9
Polina Osadcha <posadcha-ua@memsql.com>
2022-04-27 11:36:20 +0300

    Test connector with SingleStore 7.8
    Summary:
    Added singlestore-7.8 version to testing against singlestore-spark-connector
    
    **Design doc/spec**:
    **Docs impact**: none
    **Preliminary Reviewer(s)**:
    **Final Reviewer**:
    
    Test Plan: https://webapp.io/memsql/commits?query=repo%3Asinglestore-spark-connector+id%3A259
    
    Reviewers: amakarovych-ua
    
    Reviewed By: amakarovych-ua
    
    Subscribers: engineering-list
    
    JIRA Issues: PLAT-6169
    
    Differential Revision: https://grizzly.internal.memcompute.com/D56313

--- scripts/define-layerci-matrix.sh
+++ scripts/define-layerci-matrix.sh
@@ -12,22 +12,25 @@ then
 elif [ "$TEST_NUM" == '6' ] || [ "$TEST_NUM" == '7' ] || [ "$TEST_NUM" == '8' ]
 then
   echo 'export SINGLESTORE_IMAGE="memsql/cluster-in-a-box:centos-7.5.8-12c73130aa-3.2.11-1.11.11"'
+elif [ "$TEST_NUM" == '9' ] || [ "$TEST_NUM" == '10' ] || [ "$TEST_NUM" == '11' ]
+  then
+    echo 'export SINGLESTORE_IMAGE="memsql/cluster-in-a-box:centos-7.6.5-018454f4e3-4.0.1-1.13.0"'
 else
-  echo 'export SINGLESTORE_IMAGE="memsql/cluster-in-a-box:centos-7.6.5-018454f4e3-4.0.1-1.13.0"'
+  echo 'export SINGLESTORE_IMAGE="singlestore/cluster-in-a-box:alma-7.8.2-8c7b918527-4.0.4-1.13.6"'
 fi
 
-if [ "$TEST_NUM" == '12' ]
+if [ "$TEST_NUM" == '15' ]
 then
   echo 'export FORCE_READ_FROM_LEAVES=TRUE'
 else
   echo 'export FORCE_READ_FROM_LEAVES=FALSE'
 fi
 
-if [ "$TEST_NUM" == '0' ] || [ "$TEST_NUM" == '3' ] || [ "$TEST_NUM" == '6' ] || [ "$TEST_NUM" == '9' ]
+if [ "$TEST_NUM" == '0' ] || [ "$TEST_NUM" == '3' ] || [ "$TEST_NUM" == '6' ] || [ "$TEST_NUM" == '9' ] || [ "$TEST_NUM" == '12' ]
 then
   echo 'export SPARK_VERSION="3.0.0"'
   echo 'export TEST_FILTER="testOnly -- -l  ExcludeFromSpark30"'
-elif [ "$TEST_NUM" == '1' ] || [ "$TEST_NUM" == '4' ] || [ "$TEST_NUM" == '7' ] || [ "$TEST_NUM" == '10' ]
+elif [ "$TEST_NUM" == '1' ] || [ "$TEST_NUM" == '4' ] || [ "$TEST_NUM" == '7' ] || [ "$TEST_NUM" == '10' ] || [ "$TEST_NUM" == '13' ]
 then
   echo 'export SPARK_VERSION="3.1.0"'
   echo 'export TEST_FILTER="testOnly -- -l  ExcludeFromSpark31"'
