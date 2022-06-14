93d926c739de8dda58d1b5d2c3357883f73b2c00
adalbert44 <adalbert.makarovych@gmail.com>
2022-06-14 15:37:58 +0300

    debug

--- scripts/setup-cluster.sh
+++ scripts/setup-cluster.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 set -eu
-
+echo "1"
 # this script must be run from the top-level of the repo
 cd "$(git rev-parse --show-toplevel)"
+echo "2"
 
 DEFAULT_IMAGE_NAME="memsql/cluster-in-a-box:centos-7.3.2-a364d4b31f-3.0.0-1.9.3"
 IMAGE_NAME="${SINGLESTORE_IMAGE:-$DEFAULT_IMAGE_NAME}"
 CONTAINER_NAME="singlestore-integration"
+echo "3"
 
 EXISTS=$(docker inspect ${CONTAINER_NAME} >/dev/null 2>&1 && echo 1 || echo 0)
 
+echo "4"
 if [[ "${EXISTS}" -eq 1 ]]; then
   EXISTING_IMAGE_NAME=$(docker inspect -f '{{.Config.Image}}' ${CONTAINER_NAME})
   if [[ "${IMAGE_NAME}" != "${EXISTING_IMAGE_NAME}" ]]; then
@@ -18,6 +21,7 @@ if [[ "${EXISTS}" -eq 1 ]]; then
     EXISTS=0
   fi
 fi
+echo "5"
 
 if [[ "${EXISTS}" -eq 0 ]]; then
     docker run -i --init \
@@ -28,8 +32,10 @@ if [[ "${EXISTS}" -eq 0 ]]; then
         -p 5506:3306 -p 5507:3307 -p 5508:3308 \
         ${IMAGE_NAME}
 fi
+echo "6"
 
 docker start ${CONTAINER_NAME}
+echo "7"
 
 singlestore-wait-start() {
   echo -n "Waiting for SingleStore to start..."
@@ -42,8 +48,10 @@ singlestore-wait-start() {
   done
   echo ". Success!"
 }
+echo "8"
 
 singlestore-wait-start
+echo "9"
 
 if [[ "${EXISTS}" -eq 0 ]]; then
     echo
@@ -54,6 +62,7 @@ if [[ "${EXISTS}" -eq 0 ]]; then
     docker exec -it ${CONTAINER_NAME} memsqlctl start-node --yes --all
     docker exec -it ${CONTAINER_NAME} memsqlctl add-aggregator --yes --host 127.0.0.1 --password ${SINGLESTORE_PASSWORD} --port 3308
 fi
+echo "10"
 
 echo
 echo "Setting up SSL"
@@ -68,6 +77,7 @@ mysql -u root -h 127.0.0.1 -P 5506 -p"${SINGLESTORE_PASSWORD}" -e 'grant all pri
 mysql -u root -h 127.0.0.1 -P 5507 -p"${SINGLESTORE_PASSWORD}" -e 'grant all privileges on *.* to "root-ssl"@"%" require ssl with grant option'
 mysql -u root -h 127.0.0.1 -P 5508 -p"${SINGLESTORE_PASSWORD}" -e 'grant all privileges on *.* to "root-ssl"@"%" require ssl with grant option'
 echo "Done!"
+echo "11"
 
 echo
 echo "Ensuring child nodes are connected using container IP"
@@ -79,6 +89,8 @@ if [[ ${CONTAINER_IP} != "${CURRENT_LEAF_IP}" ]]; then
     # add leaf with correct ip
     mysql -u root -h 127.0.0.1 -P 5506 -p"${SINGLESTORE_PASSWORD}" --batch -N -e "add leaf root:'${SINGLESTORE_PASSWORD}'@'${CONTAINER_IP}':3307"
 fi
+echo "12"
+
 CURRENT_AGG_IP=$(mysql -u root -h 127.0.0.1 -P 5506 -p"${SINGLESTORE_PASSWORD}" --batch -N -e 'select host from information_schema.aggregators where master_aggregator=0')
 if [[ ${CONTAINER_IP} != "${CURRENT_AGG_IP}" ]]; then
     # remove aggregator with current ip
@@ -87,3 +99,5 @@ if [[ ${CONTAINER_IP} != "${CURRENT_AGG_IP}" ]]; then
     mysql -u root -h 127.0.0.1 -P 5506 -p"${SINGLESTORE_PASSWORD}" --batch -N -e "add aggregator root:'${SINGLESTORE_PASSWORD}'@'${CONTAINER_IP}':3308"
 fi
 echo "Done!"
+echo "13"
+
